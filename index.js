826a64b2437e7878f738eade923104be08860622
Jonathan Rainville <rainville.jonathan@gmail.com>
2019-01-16 15:20:16 -0500

    basic flattener

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /*global require, module*/
 
+const Flattener = require('./lib/Flattener');
+
 module.exports = (embark) => {
+  const flattener = new Flattener(embark);
+
   embark.registerConsoleCommand({
     description: "Flattens all or some of your contracts so that they can be verified on etherscan\n\t\tYou can specify which contract to flatten by using their filename (relative to the contract directory specified in embark.json). For multiple contracts, separate them using a comma",
     matches: (cmd) => {
@@ -16,9 +20,9 @@ module.exports = (embark) => {
       } else {
         embark.logger.info('Going to flatten all contracts');
       }
-      // TODO actually flatten
 
-      callback(null, "Flattening done");
+
+      flattener.flatten(contractNames, callback);
     }
   });
 };
